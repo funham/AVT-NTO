c6813e986b9c1d12a207119811d1704c4ec9339d
xsinus <fun1ham@gmail.com>
2023-02-09 16:23:34 +0300

    added PID

--- EyeCar/Car.py
+++ EyeCar/Car.py
@@ -1,18 +1,17 @@
-import numpy as np
 import cfg
 from typing import *
-
+from simple_pid import PID
 
 k_dev = cfg.MAX_ANGLE / cfg.MAX_DEVIATION
 k_break = .5
 
 angle, speed = 0, 0
 
+pid = PID(1, 0.1, 0.05, setpoint=0)
+
 
 def calc_params(deviation: float) -> Tuple[int, int]:
     global angle, speed
 
-    angle = int(deviation * k_dev)
+    angle = int(pid(deviation * k_dev))
     speed = int(cfg.MAX_SPEED - angle * k_break)
-
-    # TODO PID smoothing
--- EyeCar/cfg.py
+++ EyeCar/cfg.py
@@ -2,7 +2,7 @@ import numpy as np
 
 MAX_ANGLE: float = 45
 MAX_SPEED: int = 255
-MAX_DEVIATION: float = 5
+MAX_DEVIATION: float = .5
 
 TARGET_DISTANCE = np.inf
 CROSSROAD_STOP_DIST: float = 10
