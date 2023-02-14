c2a317d348ace56d564ee19d6c67c25265e40d1b
xsinus <fun1ham@gmail.com>
2023-02-14 15:23:36 +0300

    added emergency stop button

--- EyeCar/car-camera.py
+++ EyeCar/car-camera.py
@@ -1,5 +1,4 @@
 import cv2
-import Commander
 
 from typing import *
 from beholder2048squad.Server import Server
@@ -7,33 +6,15 @@ from beholder2048squad.Server import Server
 
 serv = Server(9090)
 
-locked = False
 
 def main_loop() -> None:
-    global locked
-
     frame = serv.chat_img()
     cv2.imshow('frame', frame)
 
-    key = cv2.waitKey(1)
-
-    if key == ord('q') or frame is None:
+    if cv2.waitKey(1) == ord('q') or frame is None:
         raise StopIteration
 
-
-    cmd = Commander.calculate_command(frame)
-    
-    if key == ord('s'):
-        locked = True
-
-    if key == ord('g'):
-        locked = False
-    
-    serv.chat_cmd(cmd if not locked else Commander.Command.STOP)
-
-    print("cmd:")
-    print(cmd)
-
+    serv.chat_cmd('')
 
 if __name__ == '__main__':
     try:
