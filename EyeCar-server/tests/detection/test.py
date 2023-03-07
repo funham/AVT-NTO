7d9256667710f61bfa73369c3b80b97ccf9e3fc7
Vlad-Lucky <vladsid0601@gmail.com>
2023-03-07 17:41:14 +0300

    test script for detection updated for yolov5detector testing

--- EyeCar-server/tests/detection/test.py
+++ EyeCar-server/tests/detection/test.py
@@ -7,9 +7,15 @@ import sys
 from pathlib import Path
 root_path = os.path.join(*Path(os.path.dirname(os.path.abspath(__file__))).parts[:-2])
 sys.path.append(root_path)
+
+from detection.detectors.yolo_detector import YoloV5Detector
 from detection.detectors.pedestrian_detector import PedestrianDetector
 
 
 model = PedestrianDetector("ai/models/sign_detector_yolov8s.pt")
+results = model.forward(cv2.imread("res/images/test_sign_detection.jpg"))
+print(results)
+
+model = YoloV5Detector("ai/models/sign_detector_yolov5s.pt")
 results = model.forward(cv2.imread("res/images/test_sign_detection.jpg"))
 print(results)
