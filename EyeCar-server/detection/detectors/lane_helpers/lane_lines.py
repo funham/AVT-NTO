8fd0daf4cf0510c34c0cc317e310a80bd3157c37
xs1nus <fun1ham@gmail.com>
2023-03-09 00:34:19 +0300

    visualized detection of a right line

--- EyeCar-server/detection/detectors/lane_helpers/lane_lines.py
+++ EyeCar-server/detection/detectors/lane_helpers/lane_lines.py
@@ -19,14 +19,14 @@ class LaneLines():
 
     def get_curvature_and_deviation(self, layout: cv2.Mat) -> tuple[float, float]:
         out_img = np.dstack((layout, layout, layout))
-        
+        h, w = layout.shape
         nwindows = 9
 
         window_h = layout.shape[0] // nwindows
-        window_w = 30
-        midpoint = int(layout.shape[1] // 2)
+        window_w = 50
+        
 
-        rlayout = layout[:, midpoint:]
+        rlayout = layout[:, w//2:]
 
         bottom_right = rlayout[-window_h:-1, :]
         hist = bottom_right.sum(axis=0) / bottom_right.max()
@@ -34,9 +34,25 @@ class LaneLines():
         line_x = [hist.argmax()]
 
         for i in range(1, nwindows):
-            hist = rlayout[-window_h*(i+1):-window_h*i]
-            line_pos = hist.argmax()
+            y1 = h - window_h * (i + 1)
+            y2 = h - window_h * i
+
+            x1 = line_x[-1]-window_w//2
+            x2 = line_x[-1]+window_w//2
+
+            hist = rlayout[y1:y2, :].sum(axis=0)            
+            max_val = hist[x1:x2].max()
+            
+            if max_val < 10:
+                last_diff = line_x[-1] - line_x[-2]
+                line_x.append(line_x[-1] + last_diff)
+                continue
+
+            line_pos = hist[x1:x2].argmax() + x1
             line_x.append(line_pos)
+
+            cv2.rectangle(out_img, (x1+w//2, y1), (x2+w//2, y2), (255,0,0), 2)
+            cv2.rectangle(out_img, (line_pos+w//2-5, y1+window_h//3), (line_pos+w//2+5, y2-window_h//3), (0,0,255), 2)
             
         cv2.imshow('out_img', out_img)
         print(line_x)
