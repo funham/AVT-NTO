2254f328d97a87179e622bec4708526cc65bad95
Vlad-Lucky <vladsid0601@gmail.com>
2023-03-12 14:40:23 +0300

    multilines detecting added

--- EyeCar-server/detection/detectors/lane_helpers/lane_lines.py
+++ EyeCar-server/detection/detectors/lane_helpers/lane_lines.py
@@ -20,43 +20,55 @@ def draw_poly(out_img: cv2.Mat, coeff_vector: np.ndarray) -> None:
 class LaneLines:
     def get_deviation_only_right(self, layout: cv2.Mat, out_img: cv2.Mat) -> float:
         img_h, img_w = layout.shape
-        line_x, line_y = self.get_line_points(layout, out_img)
-        coeff_vector = np.polyfit(line_y, line_x, deg=3)
+        lines = self.get_lines(layout, out_img)
 
-        y0 = img_h - 10
-        x0 = eval_poly(coeff_vector, y0)
+        deviations = []
+        for line in lines:
+            line_x, line_y = line[:, 0], line[:, 1]
 
-        # ain't no statistics here ;)
-        normal_deviaton = 70
-        actual_deviation = x0 - img_w // 2
+            coeff_vector = np.polyfit(line_y, line_x, deg=3)
 
-        deviation = actual_deviation - normal_deviaton
-        x_nd = img_w // 2 + normal_deviaton
+            y0 = img_h - 10
+            x0 = eval_poly(coeff_vector, y0)
 
-        if cfg.DEBUG:
-            #draw_poly(out_img, coeff_vector)
-            cv2.line(out_img, (int(x0), y0), (img_w // 2, y0), (0, 255, 255), 1, lineType=cv2.LINE_AA)
-            cv2.line(out_img, (img_w // 2, y0), (img_w // 2, y0 - 20), (0, 255, 0), 1, lineType=cv2.LINE_AA)
-            cv2.circle(out_img, (int(x_nd), y0), 5, (255, 255, 0), 1)
+            # ain't no statistics here ;)
+            normal_deviaton = 70
+            actual_deviation = x0 - img_w // 2
 
-        return 2 * deviation / img_w
+            deviation = actual_deviation - normal_deviaton
+            x_nd = img_w // 2 + normal_deviaton
 
-    def get_line_points(self, layout: cv2.Mat, out_img: cv2.Mat):
+            deviations.append(deviation)
+
+            if cfg.DEBUG:
+                draw_poly(out_img, coeff_vector)
+                cv2.line(out_img, (int(x0), y0), (img_w // 2, y0), (0, 255, 255), 1, lineType=cv2.LINE_AA)
+                cv2.line(out_img, (img_w // 2, y0), (img_w // 2, y0 - 20), (0, 255, 0), 1, lineType=cv2.LINE_AA)
+                cv2.circle(out_img, (int(x_nd), y0), 5, (255, 255, 0), 1)
+
+        return 2 * deviations[-1] / img_w
+
+    def get_lines(self, layout: cv2.Mat, out_img: cv2.Mat):
         h_layout, w_layout = layout.shape
         w_window, h_window = cfg.LANE_WINDOW_SIZE
 
-        line_x, line_y = self.get_local_line_points(layout[:, w_layout // 2:])
-        line_x += w_layout // 2
+        line_left = self.get_local_line_points(layout[:, :w_layout // 2 - 1])
 
-        for x, y in zip(line_x, line_y):
-            x1, y1 = x - w_window // 2, y - h_window // 2
-            x2, y2 = x + w_window // 2, y + h_window // 2
-            cv2.rectangle(out_img, (x1, y1), (x2, y2), (30, 30, 30), 1)
-            cv2.rectangle(out_img, (x - 5, y1), (x + 5, y2), (0, 0, 100), 1)
-        cv2.rectangle(out_img, (line_x[-1] - 5, line_y[-1] - h_window // 2),
-                      (line_x[-1] + 5, line_y[-1] + h_window // 2), (150, 130, 150), -1)
+        line_right = self.get_local_line_points(layout[:, w_layout // 2:])
+        line_right[:, 0] += w_layout // 2
 
-        return line_x, line_y
+        lines = [line_left, line_right]
+
+        for line in lines:
+            for x, y in line:
+                x1, y1 = x - w_window // 2, y - h_window // 2
+                x2, y2 = x + w_window // 2, y + h_window // 2
+                cv2.rectangle(out_img, (x1, y1), (x2, y2), (30, 30, 30), 1)
+                cv2.rectangle(out_img, (x - 5, y1), (x + 5, y2), (0, 0, 100), 1)
+            cv2.rectangle(out_img, (line[-1][0] - 5, line[-1][1] - h_window // 2),
+                          (line[-1][0] + 5, line[-1][1] + h_window // 2), (150, 130, 150), -1)
+
+        return lines
 
     def get_local_line_points(self, lane_layout: cv2.Mat):
         h_lane_layout, w_lane_layout = lane_layout.shape
@@ -125,4 +137,6 @@ class LaneLines:
         line_x = np.flip(np.array(line_x, dtype=np.int16))
         line_y = np.flip(np.array(line_y, dtype=np.int16))
 
-        return line_x, line_y
+        line = np.concatenate([np.expand_dims(line_x, axis=0), np.expand_dims(line_y, axis=0)]).T
+
+        return line
