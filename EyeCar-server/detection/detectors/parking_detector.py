e56be127beef8ce16c5efd39ac63a7a1e457b9a6
xs1nus <fun1ham@gmail.com>
2023-03-17 06:52:09 +0300

    imporoved parking detection

---
+++ EyeCar-server/detection/detectors/parking_detector.py
@@ -0,0 +1,37 @@
+"""
+Implementation of Yolo model detectors
+"""
+
+import cfg
+import cv2
+import math
+
+from include.vid_writer import VideoWriter
+from detection.detectors.yolo_detector import YoloV4Detector
+
+
+class ParkingDetector(YoloV4Detector):
+    def __init__(self, path):
+        super().__init__(path)
+
+    def forward(self, frame: cv2.Mat) -> dict:
+        classes, scores, boxes = self.model.detect(frame)
+
+        if len(boxes) == 0:
+            VideoWriter().write('parking', frame)
+            return {}
+
+        box = boxes[0]
+        print(boxes)
+
+        x, y, w, h = box
+
+        detected = frame.copy()
+
+        cv2.rectangle(detected, (x, y), (x+w, y+h), (244, 2, 232), 2, cv2.LINE_AA)
+        VideoWriter().write('parking', detected)
+
+        area = w * h
+        dist = math.sqrt(area) * .5
+
+        return {'parking_dist': dist}
